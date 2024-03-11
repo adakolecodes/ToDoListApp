29cb838043d395733bdb9c1d15adb1d87699bcdd
adakolecodes <amdeezy.com@gmail.com>
2024-03-11 23:53:14 +0100

    imported vue toast

--- src/main.js
+++ src/main.js
@@ -2,10 +2,13 @@ import { createApp } from "vue";
 import router from './router'
 import store from "./store/index.js";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Toast from "vue-toastification";
+import "vue-toastification/dist/index.css";
 import App from "./App.vue";
 
 const app = createApp(App);
 
 app.use(router)
 app.use(store);
+app.use(Toast);
 app.mount("#app");
